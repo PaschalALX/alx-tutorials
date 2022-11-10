605c7924971cc877cb8d80b76518316434dc074e
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2022-11-10 13:15:14 +0000

    Refactored _strtok.c * 2

--- simpleshell/_strtok.c
+++ simpleshell/_strtok.c
@@ -3,10 +3,34 @@
 #include <stdlib.h>
 
 /**
+ * _strtok_validate - validation for _strtok function
+ * @str: main string
+ * @backup: backup string generated in the _strtok function
+ * @delm: delimeter
+ *
+ * Return: -1 or 0
+ */
+int _strtok_validate(char *str, char *backup, char *delm)
+{
+	/* checks delimeter */
+	if (!delm)
+		return (-1);
+
+	/* checks if both backup and str have values */
+	if (backup && str)
+		return (-1);
+
+	/* checks if both backup and str are NULL */
+	if (!backup && !str)
+		return (-1);
+
+	return (0);
+}
+/**
  * _strtok - breaks a string into a sequence of
  * zero or more nonempty tokens
  * @str: string to be broken down
- * $delm: delimeter
+ * @delm: delimeter
  *
  * Return: token (char *)
  */
@@ -15,18 +39,10 @@ char *_strtok(char *str, char *delm)
 	static char *backup;
 	char *rtn = NULL;
 
-	/* checks delimeter */
-	if (!delm)
+	/* check validation */
+	if (_strtok_validate(str, backup, delm) < 0)
 		return (NULL);
 
-	/* checks if both backup and str have values */
-	if (backup && str)
-		return (NULL);
-
-	/* checks if both backup and str are NULL */
-	if (!backup && !str)
-		return (NULL);
-	
 	/* checks if backup is NULL && str has value */
 	/* Function's first call */
 	if (!backup && str)
@@ -55,6 +71,7 @@ char *_strtok(char *str, char *delm)
 	/* moves pointer over successive occurence of delimiter */
 	while (*str == *delm)
 		str++;
+
 	rtn = backup;
 	backup = str;
 	return (rtn);
